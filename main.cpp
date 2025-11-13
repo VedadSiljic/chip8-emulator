07cdc72b308b5a823da3bc508f6e8ba388594b8b
VedadSiljic <siljicvedad17@gmail.com>
2025-11-13 23:52:30 +0100

    Replace keyboard input from polling to geting direct keyboard state

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "chip-8.h"
 #include <SDL3/SDL.h>
 
-#define GAME_FILENAME "7-beep.ch8"
+#define GAME_FILENAME "6-keypad.ch8"
 #define IS_DEBUG true
 #define INSTRUCTION_DELAY 1428571 // ~700Hz
 #define TIMER_DELAY 16666666 // ~60Hz
@@ -10,13 +10,14 @@
 static int current_sine_sample = 0; // For SDL audio
 
 struct SDLApplication {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Texture* texture;
-    SDL_AudioStream* stream;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *texture;
+    SDL_AudioStream *stream;
     bool isRunning = true;
     bool isAudioRunning = false;
-    uint64_t lastInstructionTime, lastTimerTime;
+    bool previousKeyboardState[SDL_SCANCODE_COUNT] = {false};
+    uint64_t lastInstructionTime, lastTimerTime, lastInputTime;
 
     SDLApplication() {
         if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) SDL_Log("Init Error");
@@ -30,6 +31,7 @@ struct SDLApplication {
 
         lastInstructionTime = SDL_GetTicksNS();
         lastTimerTime = SDL_GetTicksNS();
+        lastInputTime = SDL_GetTicksNS();
 
         // Setup audio
         SDL_AudioSpec spec;
@@ -39,8 +41,6 @@ struct SDLApplication {
         spec.freq = 8000;
 
         stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, FeedTheAudioStreamMore, nullptr);
-        //SDL_ResumeAudioStreamDevice(stream);
-        //SDL_PauseAudioStreamDevice(stream);
 
         // Init emulator
         startChip8(GAME_FILENAME);
@@ -53,90 +53,166 @@ struct SDLApplication {
     }
 
     void Input() {
+        // limit input to once per frame
+        const uint64_t currentTime = SDL_GetTicksNS();
+        if (currentTime < lastInputTime + TIMER_DELAY) return;
+        lastInputTime = SDL_GetTicksNS();
+
         SDL_Event event;
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 isRunning = false;
-                continue;
-            }
-
-            if (event.type == SDL_EVENT_KEY_DOWN) {
-                switch (event.key.scancode) {
-                    case 30: // 1
-                        setInput(0x1);
-                        if constexpr (IS_DEBUG) SDL_Log("1 was pressed.");
-                        break;
-                    case 31: // 2
-                        setInput(0x2);
-                        if constexpr (IS_DEBUG) SDL_Log("2 was pressed.");
-                        break;
-                    case 32: // 3
-                        setInput(0x3);
-                        if constexpr (IS_DEBUG) SDL_Log("3 was pressed.");
-                        break;
-                    case 33: // 4
-                        setInput(0xC);
-                        if constexpr (IS_DEBUG) SDL_Log("4 was pressed.");
-                        break;
-                    case 20: // Q
-                        setInput(0x4);
-                        if constexpr (IS_DEBUG) SDL_Log("Q was pressed.");
-                        break;
-                    case 26: // W
-                        setInput(0x5);
-                        if constexpr (IS_DEBUG) SDL_Log("W was pressed.");
-                        break;
-                    case 8: // E
-                        setInput(0x6);
-                        if constexpr (IS_DEBUG) SDL_Log("E was pressed.");
-                        break;
-                    case 21: // R
-                        setInput(0xD);
-                        if constexpr (IS_DEBUG) SDL_Log("R was pressed.");
-                        break;
-                    case 4: // A
-                        setInput(0x7);
-                        if constexpr (IS_DEBUG) SDL_Log("A was pressed.");
-                        break;
-                    case 22: // S
-                        setInput(0x8);
-                        if constexpr (IS_DEBUG) SDL_Log("S was pressed.");
-                        break;
-                    case 7: // D
-                        setInput(0x9);
-                        if constexpr (IS_DEBUG) SDL_Log("D was pressed.");
-                        break;
-                    case 9: // F
-                        setInput(0xE);
-                        if constexpr (IS_DEBUG) SDL_Log("F was pressed.");
-                        break;
-                    case 29: // Z
-                        setInput(0xA);
-                        if constexpr (IS_DEBUG) SDL_Log("Z was pressed.");
-                        break;
-                    case 27: // X
-                        setInput(0x0);
-                        if constexpr (IS_DEBUG) SDL_Log("X was pressed.");
-                        break;
-                    case 6: // C
-                        setInput(0xB);
-                        if constexpr (IS_DEBUG) SDL_Log("C was pressed.");
-                        break;
-                    case 25: // V
-                        setInput(0xF);
-                        if constexpr (IS_DEBUG) SDL_Log("V was pressed.");
-                        break;
-                    default:
-                        if constexpr (IS_DEBUG) SDL_Log("A key was pressed : %d", event.key.scancode);
-                }
-
-                continue;
-            }
-
-            if (event.type == SDL_EVENT_KEY_UP) {
-                setInput(0xFF);
             }
         }
+
+        const bool *currentKeyboardState = SDL_GetKeyboardState(nullptr);
+
+        // Row 1
+        if (currentKeyboardState[SDL_SCANCODE_1]) {
+            setInput(0x1);
+            if constexpr (IS_DEBUG) SDL_Log("1 was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_2]) {
+            setInput(0x2);
+            if constexpr (IS_DEBUG) SDL_Log("2 was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_3]) {
+            setInput(0x3);
+            if constexpr (IS_DEBUG) SDL_Log("3 was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_4]) {
+            setInput(0xC);
+            if constexpr (IS_DEBUG) SDL_Log("4 was pressed.");
+        }
+
+        // Row 2
+        if (currentKeyboardState[SDL_SCANCODE_Q]) {
+            setInput(0x4);
+            if constexpr (IS_DEBUG) SDL_Log("Q was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_W]) {
+            setInput(0x5);
+            if constexpr (IS_DEBUG) SDL_Log("W was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_E]) {
+            setInput(0x6);
+            if constexpr (IS_DEBUG) SDL_Log("E was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_R]) {
+            setInput(0xD);
+            if constexpr (IS_DEBUG) SDL_Log("R was pressed.");
+        }
+
+        // Row 3
+        if (currentKeyboardState[SDL_SCANCODE_A]) {
+            setInput(0x7);
+            if constexpr (IS_DEBUG) SDL_Log("A was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_S]) {
+            setInput(0x8);
+            if constexpr (IS_DEBUG) SDL_Log("S was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_D]) {
+            setInput(0x9);
+            if constexpr (IS_DEBUG) SDL_Log("D was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_F]) {
+            setInput(0xE);
+            if constexpr (IS_DEBUG) SDL_Log("F was pressed.");
+        }
+
+        // Row 4
+        if (currentKeyboardState[SDL_SCANCODE_Z]) {
+            setInput(0xA);
+            if constexpr (IS_DEBUG) SDL_Log("Z was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_X]) {
+            setInput(0x0);
+            if constexpr (IS_DEBUG) SDL_Log("X was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_C]) {
+            setInput(0xB);
+            if constexpr (IS_DEBUG) SDL_Log("C was pressed.");
+        }
+        if (currentKeyboardState[SDL_SCANCODE_V]) {
+            setInput(0xF);
+            if constexpr (IS_DEBUG) SDL_Log("V was pressed.");
+        }
+
+        // Row 1
+        if (previousKeyboardState[SDL_SCANCODE_1] && !currentKeyboardState[SDL_SCANCODE_1]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("1 was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_2] && !currentKeyboardState[SDL_SCANCODE_2]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("2 was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_3] && !currentKeyboardState[SDL_SCANCODE_3]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("3 was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_4] && !currentKeyboardState[SDL_SCANCODE_4]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("4 was released.");
+        }
+
+        // Row 2
+        if (previousKeyboardState[SDL_SCANCODE_Q] && !currentKeyboardState[SDL_SCANCODE_Q]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("Q was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_W] && !currentKeyboardState[SDL_SCANCODE_W]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("W was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_E] && !currentKeyboardState[SDL_SCANCODE_E]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("E was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_R] && !currentKeyboardState[SDL_SCANCODE_R]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("R was released.");
+        }
+
+        // Row 3
+        if (previousKeyboardState[SDL_SCANCODE_A] && !currentKeyboardState[SDL_SCANCODE_A]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("A was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_S] && !currentKeyboardState[SDL_SCANCODE_S]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("S was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_D] && !currentKeyboardState[SDL_SCANCODE_D]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("D was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_F] && !currentKeyboardState[SDL_SCANCODE_F]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("F was released.");
+        }
+
+        // Row 4
+        if (previousKeyboardState[SDL_SCANCODE_Z] && !currentKeyboardState[SDL_SCANCODE_Z]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("Z was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_X] && !currentKeyboardState[SDL_SCANCODE_X]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("X was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_C] && !currentKeyboardState[SDL_SCANCODE_C]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("C was released.");
+        }
+        if (previousKeyboardState[SDL_SCANCODE_V] && !currentKeyboardState[SDL_SCANCODE_V]) {
+            setInput(0xFF);
+            if constexpr (IS_DEBUG) SDL_Log("V was released.");
+        }
+
+        for (int i = 0; i < SDL_SCANCODE_COUNT; i++) previousKeyboardState[i] = currentKeyboardState[i];
     }
 
     void Update() {
@@ -154,18 +230,18 @@ struct SDLApplication {
         if (runAudio() && !isAudioRunning) {
             isAudioRunning = true;
             SDL_ResumeAudioStreamDevice(stream);
-        }
-        else if (!runAudio()) {
+        } else if (!runAudio()) {
             isAudioRunning = false;
             SDL_PauseAudioStreamDevice(stream);
         }
     }
 
     void Render() const {
-        const bool* display = getDisplay();
+        const bool *display = getDisplay();
         uint32_t pixels[64 * 32];
 
-        for (int i = 0; i < 64 * 32; i++) pixels[i] = display[i] ? 0xFFFFFFFF : 0xFF000000; // set the pixel white or black
+        for (int i = 0; i < 64 * 32; i++) pixels[i] = display[i] ? 0xFFFFFFFF : 0xFF000000;
+        // set the pixel white or black
         SDL_UpdateTexture(texture, nullptr, pixels, 64 * sizeof(uint32_t));
 
         SDL_RenderClear(renderer);
@@ -179,11 +255,11 @@ struct SDLApplication {
         }
     }
 
-    static void SDLCALL FeedTheAudioStreamMore(void *userdata, SDL_AudioStream *astream, int additional_amount, int total_amount) {
-
-        additional_amount /= sizeof (float);  /* convert from bytes to samples */
+    static void SDLCALL FeedTheAudioStreamMore(void *userdata, SDL_AudioStream *astream, int additional_amount,
+                                               int total_amount) {
+        additional_amount /= sizeof(float); /* convert from bytes to samples */
         while (additional_amount > 0) {
-            float samples[128];  /* this will feed 128 samples each iteration until we have enough. */
+            float samples[128]; /* this will feed 128 samples each iteration until we have enough. */
             const int total = SDL_min(additional_amount, SDL_arraysize(samples));
             int i;
 
@@ -200,8 +276,8 @@ struct SDLApplication {
             current_sine_sample %= 8000;
 
             /* feed the new data to the stream. It will queue at the end, and trickle out as the hardware needs more data. */
-            SDL_PutAudioStreamData(astream, samples, total * sizeof (float));
-            additional_amount -= total;  /* subtract what we've just fed the stream. */
+            SDL_PutAudioStreamData(astream, samples, total * sizeof(float));
+            additional_amount -= total; /* subtract what we've just fed the stream. */
         }
     }
 
@@ -212,7 +288,6 @@ struct SDLApplication {
         SDL_DestroyAudioStream(stream);
         SDL_Quit();
     }
-
 };
 
 int main() {
